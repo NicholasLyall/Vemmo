b59d80d3034879c3b4f3b9e2368742c1f7050ddf
djm5ms <djm5ms@virginia.edu>
2025-11-09 23:20:10 -0500

    made .env stuff and connection w db

--- vemmo_backend/main.py
+++ vemmo_backend/main.py
@@ -1,4 +1,16 @@
 from fastapi import FastAPI, APIRouter
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv('.env.local')  # Load env variables from .env.local
+
+SUPABASE_URL = os.getenv("EXPO_PUBLIC_SUPABASE_URL")
+SUPABASE_KEY = os.getenv("EXPO_PUBLIC_SUPABASE_KEY")
+
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 
 app = FastAPI(debug=True)
 router = APIRouter()
@@ -8,5 +20,8 @@ router = APIRouter()
 
 @router.get("/payment_info")
 async def get_payment_info(payment_id: str):
+    
+    
+    
     return {"payment_id": payment_id, "status": "Processed"}
 
